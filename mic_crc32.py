bd01a4156bc235cdb41202b3ead8631f02728f60
Shoichi Sakane <github-shoichi@tanu.org>
2018-03-23 23:52:23 +0900

    supported crc32 for micropython.

--- mic_crc32.py
+++ mic_crc32.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import zlib
+import sys
 
-def get_mic(data):
-    '''
-    data: a bytes-like object.
-    return
-        an integer of the mic, which is decoded into 32 bits.
-        size of the mic, i.e. 32
-    '''
-    return zlib.crc32(data), 32
+if "implementation" in dir(sys) and sys.implementation.name == "micropython":
+    from ubinascii import crc32
+    def get_mic(data):
+        return crc32(data), 32
+else:
+    from binascii import crc32
+    def get_mic(data):
+        '''
+        data: a bytes-like object.
+        return
+            an integer of the mic, which is decoded into 32 bits.
+            size of the mic, i.e. 32
+        '''
+        return crc32(data), 32
+
