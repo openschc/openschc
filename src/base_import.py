40e4d008e352570c85af407cfdab2cc8c5118ce5
Cedric Adjih <Cedric.Adjih@inria.fr>
2018-11-03 02:00:34 +0700

    added slow, straightforward scheduler

--- src/base_import.py
+++ src/base_import.py
@@ -1,9 +1,24 @@
-# Import all external modules, with proper try/except in order to allow
+# Import all external modules with proper try/except in order to allow
 # for code running on both Micropython and Python
+# Import some internal modules (fake/temporary versions)
+
+import sys
 
 try:
     import time
 except ImportError:
     import utime as time
+
+# --- sched
+#sys.path.append("../../micropython-lib/heapq") # XXX (for pyssched)
+#sys.path.append("../../micropython-lib/ffilib") # XXX (for time)
+#sys.path.append("../../micropython-lib/time") # XXX (for pyssched)
+
+###sys.path.append("../../micropython-lib/sched") # XXX
+###import sched # empty...
+#sys.path.append("schctest/pyssched")
+#import pyssched as sched
+
+# --- default imports
 
 from bitarray import FakeBitBuffer as BitBuffer
--- src/simsched.py
+++ src/simsched.py
@@ -3,9 +3,43 @@
 
 from base_import import *  # used for now for differing modules in py/upy
 
-class SimulScheduler:
+
+class SlowSimulScheduler:
     def __init__(self):
-        self.scheduler = sched.scheduler(self.get_clock, self._wait_delay)
+        self.queue = []
+        self.clock = 0
+        self.event_id = 0
+
+    # sched.scheduler API
+
+    def get_clock(self):
+        return self.clock
+
+    def _wait_delay(self, delay):
+        self.clock += delay
+
+    def run(self):
+        while len(self.queue) > 0:
+            self.queue.sort()
+            self.clock, event_id, callback, args = self.queue.pop(0)
+            callback(*args)
+
+    # external API
+
+    def add_event(self, rel_time, callback, args):
+        assert rel_time >= 0
+        clock = self.get_clock()
+        abs_time = clock+rel_time
+        self.queue.append((abs_time, self.event_id, callback, args))
+        self.event_id += 1
+
+SimulScheduler = SlowSimulScheduler
+
+
+class SimulSchedulerOld:
+    def __init__(self):
+        #self.scheduler = sched.scheduler(self.get_clock, self._wait_delay)
+        self.scheduler = sched.ssched(self.get_clock)
         self.clock = 0
 
     # sched.scheduler API
@@ -17,14 +51,10 @@ class SimulScheduler:
         self.clock += delay
 
     def run(self):
-        self.scheduler.run()
+        #self.scheduler.run()
+        self.scheduler.execute()
 
     # external API
 
     def add_event(self, rel_time, callback, args):
         self.scheduler.enter(rel_time, 0, callback, args)
-
-
-class RealTimeScheduler:
-    """XXX.TODO"""
-    pass
