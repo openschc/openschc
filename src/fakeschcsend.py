69618089f3cb97c6b13372b66685ce1dc35092c5
Cedric Adjih <Cedric.Adjih@inria.fr>
2018-11-04 01:07:21 +0700

    started refactoring of schc_fragment_holder

--- src/fakeschcsend.py
+++ src/fakeschcsend.py
@@ -2,86 +2,49 @@
 from base_import import *  # used for now for differing modules in py/upy
 
 import schc
+import schcmsg
 
-# https://raw.githubusercontent.com/wiki/openschc/doc/draft-ietf-lpwan-ipv6-static-context-hc-17-ref.txt
+rule_as_dict = {
+    # Header format
+    "rule-id-size": 6,
+    "dtag-size": 2,
+    "window-size": 5,
+    "fcn-size": 3,
+    "mode": "ack-on-error",
 
-# 1434 8.3.1.1.  Regular SCHC Fragment
-# 1435
-# 1436    The Regular SCHC Fragment format is shown in Figure 11.  Regular SCHC
-# 1437    Fragments are generally used to carry tiles that are not the last one
-# 1438    of a SCHC Packet.  The DTag field and the W field are optional.
-# 1439
-# 1440  |--- SCHC Fragment Header ----|
-# 1441            |-- T --|-M-|-- N --|
-# 1442  +-- ... --+- ... -+---+- ... -+--------...-------+~~~~~~~~~~~~~~~~~~~~~
-# 1443  | Rule ID | DTag  | W |  FCN  | Fragment Payload | padding (as needed)
-# 1444  +-- ... --+- ... -+---+- ... -+--------...-------+~~~~~~~~~~~~~~~~~~~~~
+    "tile-size": 30,
+    "mic-algorithm": "crc32"
+}
 
-# 1517   |---- SCHC ACK Header ----|
-# 1518               |-- T --|-M-|1|
-# 1519   +---- ... --+- ... -+---+-+~~~~~~~~~~~~~~~~~~
-# 1520   |  Rule ID  |  DTag | W |1| padding as needed                (success)
-# 1521   +---- ... --+- ... -+---+-+~~~~~~~~~~~~~~~~~~
+#---------------------------------------------------------------------------
+# XXX: put rule_manager
 
-sys.path.append("schctest")
-#import
-import schc_fragment_holder
+Rule = namedtuple("Rule", "rule_id_size dtag_size window_size"
+                  + " fcn_size mode tile_size mic_algorithm")
 
-#schc_fragment_holder.XXX
+def rule_from_dict(rule_as_dict):
+    rule_as_dict = { k.replace("-","_"): v for k,v in rule_as_dict.items() }
+    return Rule(**rule_as_dict)
 
-
-RULE_ID_SIZE = 6
-DTAG_SIZE = 2
-WINDOW_SIZE = 5
-FCN_SIZE = 3
-
-# the above parameters allow having a header aligned on byte boundaries
-MAX_WIND_FCN = 6
-TILE_SIZE = 30
-
-RULE_ID = 9
-DTAG = 1
-
-# XXX:
-FCN_ALL_0 = 0
-
-class Struct:
-    pass
-
-RULE = Struct()
-RULE.rid = RULE_ID
-RULE.C = Struct()
-RULE.C.rid_size =  RULE_ID_SIZE
-RULE.dtag_size = DTAG_SIZE
-RULE.win_size = WINDOW_SIZE
-RULE.fcn_size = FCN_SIZE
-PAYLOAD_SIZE = 30
+#---------------------------------------------------------------------------
 
 class FakeSCHCProtocolSender(schc.SCHCProtocol):
 
     def start_sending(self):
-        #packet = b""
-        fragment = schc_fragment_holder.frag_sender_tx(
-            R=RULE,
-            dtag=DTAG, win=0, fcn=6, mic=None, bitmap=None,
+        return
+        fragment = schcmsg.frag_sender_tx(
+            R=rule, dtag=DTAG, win=0, fcn=6, mic=None, bitmap=None,
             cbit=None, payload=None)
-        #fragment.set_param(rid=, dtag=, win=, fcn=, mic=None, bitmap=None,
-        #                   cbit=None, payload=b"")
-        #fragment.init_param()
         print("->", fragment.dump())
-        #print(fragment)
-        #self.layer2.send_packet(fragment)
 
+def make_test_fragment_list(rule):
+    rule_id = 9
+    dtag = 1
+    W = 0
+    one_tile = 30 * b"\0"
+    message = schcmsg.frag_sender_tx(rule, rule_id, dtag, W, one_tile)
 
-def fake_schc_packet(config, rule, payload):
-    fragment = schc_fragment_holder.frag_sender_tx(
-        R=RULE,
-        dtag=DTAG, win=0, fcn=6, mic=None, bitmap=None,
-        cbit=None, payload=payload)
-    return fragment
-
-
-fake_packet = fake_schc_packet(None, None, "0"*30)
-print(fake_packet.dump())
-print(fake_packet.full_dump())
+rule = rule_from_dict(rule_as_dict)
+print(rule)
+print(make_test_fragment_list(rule))
 
--- src/schcmsg.py
+++ src/schcmsg.py
@@ -1,8 +1,38 @@
 
-#from __future__ import print_function
+from base_import import *  # used for now for differing modules in py/upy
 
-import pybinutil as pb
-from schc_param import *
+# Rule format contains the following fields:
+# "rule-id-size", "dtag-size", "window-size", "fcn-size", "mode",
+# "tile-size", "mic-algorithm"
+
+# Copied from schctest/schc_param.py
+
+DEFAULT_FRAGMENT_RID = 1
+DEFAULT_L2_SIZE = 8
+DEFAULT_RECV_BUFSIZE = 512
+DEFAULT_TIMER_T1 = 5
+DEFAULT_TIMER_T2 = 10
+DEFAULT_TIMER_T3 = 10
+DEFAULT_TIMER_T4 = 12
+DEFAULT_TIMER_T5 = 14
+
+#class SCHC_MODE(Enum)
+class SCHC_MODE():
+    NO_ACK = 0
+    ACK_ALWAYS = 1
+    ACK_ON_ERROR = 2
+
+#---------------------------------------------------------------------------
+
+# 1372       *  The FCN value with all the bits equal to 1 (called All-1)
+# 1373          signals the very last tile of a SCHC Packet.  By extension, if
+# 1374          windows are used, the last window of a packet is called the
+# 1375          All-1 window.
+
+def get_all_1(rule):
+    return 
+
+#---------------------------------------------------------------------------
 
 '''
 NOTE:
@@ -10,13 +40,13 @@ NOTE:
     XXX how do I have to handle the padding ?
 '''
 
-class frag_holder():
+class frag_base():
     '''
     base class for operation of message.
     packet: a bytearray transmitted or a bytearray received.
     '''
     def init_param(self):
-        self.rid = None
+        self.rule_id = None
         self.dtag = None
         self.win = None
         self.fcn = None
@@ -26,8 +56,8 @@ class frag_holder():
         self.payload = None
         self.packet = None
 
-    def set_param(self, rid, dtag, win, fcn, mic, bitmap, cbit, payload):
-        self.rid = rid
+    def set_param(self, rule_id, dtag, win, fcn, mic, bitmap, cbit, payload):
+        self.rule_id = rule_id
         self.dtag = dtag
         self.win = win
         self.fcn = fcn
@@ -36,25 +66,25 @@ class frag_holder():
         self.cbit = cbit
         self.payload = payload
         # check the field size.
-        if dtag > (2**self.R.dtag_size) - 1:
+        if dtag > (2**self.rule.dtag_size) - 1:
             raise ValueError("dtag is too big than the field size.")
 
     def dump(self):
         x = ""
-        if self.rid != None:
-            x += ("rid:%s" % pb.int_to_bit(self.rid, self.R.C.rid_size))
+        if self.rule_id != None:
+            x += ("rule_id:%s" % pb.int_to_bit(self.rule_id, self.rule.rule_id_size))
         if self.dtag != None:
             if len(x) != 0: x += " "
-            x += ("dtag:%s" % pb.int_to_bit(self.dtag, self.R.dtag_size))
+            x += ("dtag:%s" % pb.int_to_bit(self.dtag, self.rule.dtag_size))
         if self.win != None:
             if len(x) != 0: x += " "
             x += ("w:%d" % self.win)
         if self.fcn != None:
             if len(x) != 0: x += " "
-            x += ("fcn:%s" % pb.int_to_bit(self.fcn, self.R.fcn_size))
+            x += ("fcn:%s" % pb.int_to_bit(self.fcn, self.rule.fcn_size))
         if self.mic != None:
             if len(x) != 0: x += " "
-            x += ("mic:%s" % pb.int_to_bit(self.mic, self.R.C.mic_size))
+            x += ("mic:%s" % pb.int_to_bit(self.mic, self.rule.mic_size))
             x += ("(0x%s)" % "".join(["%02x"%((self.mic>>i)&0xff)
                                      for i in [24,16,8,0]]))
         if self.cbit != None:
@@ -62,7 +92,7 @@ class frag_holder():
             x += ("cbit:%d" % self.cbit)
         if self.bitmap != None:
             if len(x) != 0: x += " "
-            x += ("bitmap:%s" % pb.int_to_bit(self.bitmap, self.R.bitmap_size))
+            x += ("bitmap:%s" % pb.int_to_bit(self.bitmap, self.rule.bitmap_size))
         if self.payload != None:
             if len(x) != 0: x += " "
             x += ("payload:%s" % self.payload)
@@ -72,7 +102,7 @@ class frag_holder():
     def full_dump(self):
         return " ".join(["%02x"%i for i in self.packet])
 
-class frag_tx(frag_holder):
+class frag_tx(frag_base):
 
     '''
     parent class for sending message.
@@ -80,65 +110,52 @@ class frag_tx(frag_holder):
     def make_frag(self, dtag, win=None, fcn=None, mic=None, bitmap=None,
                   cbit=None, abort=False, payload=None):
         '''
-        payload: bit string of the SCHC fragment payload.
+        payload: bytearray of the SCHC fragment payload.
         '''
         #
-        ba = bytearray()
-        pos = 0
+        buffer = BitBuffer()
         #
         # basic fields.
-        if self.R.rid != None and self.R.C.rid_size:
-            pb.bit_set(ba, 0, pb.int_to_bit(self.R.rid, self.R.C.rid_size),
-                       extend=True)
-            pos += self.R.C.rid_size
-        if dtag != None and self.R.dtag_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(dtag, self.R.dtag_size),
-                       extend=True)
-            pos += self.R.dtag_size
+        if self.rule_id is not None and self.rule.rule_id_size is not None:
+            buffer.add_bits(self.rule_id, self.rule.rule_id_size)
+        if dtag is not None and self.rule.dtag_size is not None:
+            assert self.rule.dtag_size != None # CA: sanity check
+            buffer.add_bits(dtag, self.rule.dtag_size)
         #
         # extension fields.
-        if win != None and self.R.win_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(win, self.R.win_size),
-                       extend=True)
-            pos += self.R.win_size
-        if fcn != None and self.R.fcn_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(fcn, self.R.fcn_size),
-                       extend=True)
-            pos += self.R.fcn_size
-        if mic != None and self.R.C.mic_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(mic, self.R.C.mic_size),
-                       extend=True)
-            pos += self.R.C.mic_size
-        if cbit != None and self.R.cbit_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(cbit, self.R.cbit_size),
-                       extend=True)
-            pos += self.R.cbit_size
-        if bitmap != None and self.R.bitmap_size:
-            pb.bit_set(ba, pos, pb.int_to_bit(bitmap, self.R.bitmap_size),
-                       extend=True)
-            pos += self.R.bitmap_size
+        if win is not None and self.rule.window_size is not None:
+            buffer.add_bits(win, self.rule.window_size)
+        if fcn is not None and self.rule.fcn_size is not None:
+            buffer.add_bits(fcn, self.rule.fcn_size)
+        if mic != None and self.rule.C.mic_size is not None:
+            buffer.add_bits(mic, self.rule.C.mic_size)
+        if cbit != None and self.rule.cbit_size:
+            buffer.add_bits(cbit, self.rule.cbit_size)
+        if bitmap != None and self.rule.bitmap_size:
+            buffer.add_bits(bitmap, self.rule.bitmap_size)
         if abort == True:
-            pb.bit_set(ba, pos, pb.int_to_bit(0xff, 8),
-                       extend=True)
+            raise RunTimeError("not refactored", "abort == True")
+            pb.bit_set(ba, pos, pb.int_to_bit(0xff, 8), extend=True)
             pos += 8
         #
         if payload != None:
-            # assumed that bit_set() has extended to a byte boundary.
-            pb.bit_set(ba, pos, payload, extend=True)
+            # assumed that bit_set() has extended to a byte boundary
+            buffer.add_bytes(payload)
         #
         # the abort field is implicit, is not needed to set into the parameter.
-        self.set_param(self.R.rid, dtag, win, fcn, mic, bitmap, cbit, payload)
-        self.packet = ba
+        self.set_param(self.rule_id, dtag, win, fcn, mic, bitmap, cbit, payload)
+        self.packet = buffer
 
 class frag_sender_tx(frag_tx):
 
     '''
     for the fragment sender to send a message.
     '''
-    def __init__(self, R, dtag, win=None, fcn=None, mic=None, bitmap=None,
-                 cbit=None, payload=None):
+    def __init__(self, rule, rule_id, dtag, win=None, fcn=None, mic=None,
+                 bitmap=None, cbit=None, payload=None):
         self.init_param()
-        self.R = R
+        self.rule = rule
+        self.rule_id = rule_id
         self.make_frag(dtag, win=win, fcn=fcn, mic=mic, bitmap=bitmap,
                        cbit=cbit, payload=payload)
 
@@ -150,7 +167,7 @@ class frag_receiver_tx_all0_ack(frag_tx):
     '''
     def __init__(self, R, dtag, win=None, bitmap=None):
         self.init_param()
-        self.R = R
+        self.rule = R
         self.make_frag(dtag, win=win, bitmap=bitmap)
 
 class frag_receiver_tx_all1_ack(frag_tx):
@@ -162,7 +179,7 @@ class frag_receiver_tx_all1_ack(frag_tx):
     '''
     def __init__(self, R, dtag, win=None, cbit=None, bitmap=None):
         self.init_param()
-        self.R = R
+        self.rule = R
         self.make_frag(dtag, win=win, cbit=cbit, bitmap=bitmap)
 
 class frag_receiver_tx_abort(frag_tx):
@@ -173,10 +190,10 @@ class frag_receiver_tx_abort(frag_tx):
     '''
     def __init__(self, R, dtag, win=None, cbit=None, bitmap=None):
         self.init_param()
-        self.R = R
+        self.rule = R
         self.make_frag(dtag, win=win, abort=True)
 
-class frag_rx(frag_holder):
+class frag_rx(frag_base):
 
     '''
     parent class for receiving message.
@@ -190,81 +207,81 @@ class frag_rx(frag_holder):
         else:
             raise TypeError("recvbuf must be str, bytes or bytearray.")
 
-    def parse_rid(self, C, exp_rid=None):
+    def parse_rule_id(self, C, exp_rule_id=None):
         '''
-        parse rid in the frame.
-        exp_rid: if non-None, check the rid whether it's expected.
+        parse rule_id in the frame.
+        exp_rule_id: if non-None, check the rule_id whether it's expected.
         '''
-        if C.rid_size:
-            rid = pb.bit_get(self.packet, 0, C.rid_size, ret_type=int)
-            if exp_rid != None and rid != exp_rid:
-                raise ValueError("rid unexpected.")
+        if C.rule_id_size:
+            rule_id = pb.bit_get(self.packet, 0, C.rule_id_size, ret_type=int)
+            if exp_rule_id != None and rule_id != exp_rule_id:
+                raise ValueError("rule_id unexpected.")
         else:
-            rid = C.default_rid
+            rule_id = C.default_rule_id
         #
-        self.rid = rid
-        return C.rid_size
+        self.rule_id = rule_id
+        return C.rule_id_size
 
     def parse_dtag(self, pos, exp_dtag=None):
         '''
         parse dtag in the frame.
         exp_dtag: if non-None, check the dtag whether it is expected.
         '''
-        if self.R.dtag_size:
-            dtag = pb.bit_get(self.packet, pos, self.R.dtag_size, ret_type=int)
+        if self.rule.dtag_size:
+            dtag = pb.bit_get(self.packet, pos, self.rule.dtag_size, ret_type=int)
             if exp_dtag != None and dtag != exp_dtag:
                 raise ValueError("dtag unexpected.")
         else:
-            dtag = self.R.C.default_dtag
+            dtag = self.rule.C.default_dtag
         #
         self.dtag = dtag
-        return self.R.dtag_size
+        return self.rule.dtag_size
 
     def parse_win(self, pos, exp_win=None):
         '''
         parse win in the frame.
         exp_win: if non-None, check the win whether it is expected.
-        if win_size is zero, self.win is not set.
+        if window_size is zero, self.win is not set.
         '''
-        if self.R.win_size:
-            win = pb.bit_get(self.packet, pos, self.R.win_size, ret_type=int)
+        if self.rule.window_size:
+            win = pb.bit_get(self.packet, pos, self.rule.window_size, ret_type=int)
             if exp_win != None and win != exp_win:
                 raise ValueError("the value of win unexpected. win=%d expected=%d" % (win, exp_win))
             self.win = win
-        return self.R.win_size
+        return self.rule.window_size
 
     def parse_fcn(self, pos):
         '''
         parse fcn in the frame.
         assuming that fcn_size is not zero.
         '''
-        self.fcn = pb.bit_get(self.packet, pos, self.R.fcn_size, ret_type=int)
-        return self.R.fcn_size
+        self.fcn = pb.bit_get(self.packet, pos, self.rule.fcn_size, ret_type=int)
+        return self.rule.fcn_size
 
     def parse_bitmap(self, pos):
         '''
         parse bitmap in the frame.
         assuming that bitmap_size is not zero.
         '''
-        self.bitmap = pb.bit_get(self.packet, pos, self.R.bitmap_size,
+        self.bitmap = pb.bit_get(self.packet, pos, self.rule.bitmap_size,
                                  ret_type=int)
-        return self.R.bitmap_size
+        return self.rule.bitmap_size
 
     def parse_cbit(self, pos):
         '''
         parse cbit in the frame.
         assuming that cbit_size is not zero.
         '''
-        self.cbit = pb.bit_get(self.packet, pos, self.R.cbit_size, ret_type=int)
-        return self.R.cbit_size
+        self.cbit = pb.bit_get(self.packet, pos, self.rule.cbit_size, ret_type=int)
+        return self.rule.cbit_size
 
     def parse_mic(self, pos):
         '''
         parse mic in the frame.
         assuming that mic_size is not zero.
         '''
-        self.mic = pb.bit_get(self.packet, pos, self.R.C.mic_size, ret_type=int)
-        return self.R.C.mic_size
+        self.mic = pb.bit_get(self.packet, pos, self.rule.C.mic_size, ret_type=int)
+        return self.rule.C.mic_size
 
 class frag_sender_rx_all0_ack(frag_rx):
 
@@ -287,9 +304,9 @@ class frag_sender_rx_all0_ack(frag_rx):
         '''
         self.init_param()
         self.set_recvbuf(recvbuf)
-        self.R = R
+        self.rule = R
         pos = 0
-        pos += self.parse_rid(self.R.C, exp_rid=self.R.rid)
+        pos += self.parse_rule_id(self.rule.C, exp_rule_id=self.rule.rule_id)
         pos += self.parse_dtag(pos, exp_dtag=dtag)
         pos += self.parse_win(pos, exp_win=win)
         # XXX the abort is not supported yet.
@@ -310,9 +327,9 @@ class frag_sender_rx_all1_ack(frag_rx):
         '''
         self.init_param()
         self.set_recvbuf(recvbuf)
-        self.R = R
+        self.rule = R
         pos = 0
-        pos += self.parse_rid(self.R.C, exp_rid=self.R.rid)
+        pos += self.parse_rule_id(self.rule.C, exp_rule_id=self.rule.rule_id)
         pos += self.parse_dtag(pos, exp_dtag=dtag)
         pos += self.parse_win(pos, exp_win=win)
         # XXX the abort is not supported yet.
@@ -344,15 +361,15 @@ class frag_receiver_rx(frag_rx):
         self.init_param()
         self.set_recvbuf(recvbuf)
         self.__pos = 0
-        self.__pos += self.parse_rid(C)
+        self.__pos += self.parse_rule_id(C)
 
     def finalize(self, R):
-        self.R = R
+        self.rule = R
         pos = self.__pos
         pos += self.parse_dtag(pos)
         pos += self.parse_win(pos)
         pos += self.parse_fcn(pos)
-        if self.fcn == self.R.fcn_all_1:
+        if self.fcn == self.rule.fcn_all_1:
             pos += self.parse_mic(pos)
         payload_bit_len = len(self.packet)*8 - pos
         self.payload = pb.bit_get(self.packet, pos, payload_bit_len)
