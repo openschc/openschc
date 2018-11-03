d460e9b37bb6a3586164258d61b058a16522ea11
dbarthel-ol <dominique.barthel@orange.com>
2018-11-03 18:01:33 +0700

    Update rulemanager.py
    some text cleanup

--- src/rulemanager.py
+++ src/rulemanager.py
@@ -2,27 +2,27 @@
 This module is used to manage rules. A rule is defined in JSON and is uniquely
 identified by a rule ID of variable length.
 
-Each rule must contains the following informations:
+Each rule must contain the following information:
 
 {
   "ruleID" : 2,
-  "ruleLenght" : 3
+  "ruleLength" : 3
 }
 
 where ruleID contains the rule ID value aligned on the right and ruleLength gives 
-the size in bits of the ruleID. In the previous example, this correspond to the 
+the size in bits of the ruleID. In the previous example, this corresponds to the 
 binary value 0b010.
 
 The rule is either a compression/decompression rule or a fragmentation/reassembly
 rule.
 
-This C/D rules, the keyword "fragmentation" must be defined. For F/R rules, the
-keyword "compression" must be used.
+For C/D rules, the keyword "compression" must be defined. For F/R rules, the
+keyword "fragmentation" must be defined.
 
 For instance:
 {
   "ruleID" : 14,
-  "ruleLenght" : 4   # rule 0b1110
+  "ruleLength" : 4   # rule 0b1110
   "fragmentation": <<<rule>>>
 }
 
@@ -30,8 +30,8 @@ where <<<rule>>> will be defined later.
 
 {
   "ruleID" : 15,
-  "ruleLenght" : 4   # rule 0b1110
-  "compression": {
+  "ruleLength" : 4   # rule 0b1110
+  "fragmentation": {
       "dtagSize" : 1,
       "windowSize": 3,
       "FCNSize" : 3,
@@ -43,10 +43,10 @@ where <<<rule>>> will be defined later.
   }
 }
 
-The "compression" keyword is used to gives compression paramters and profile:
+The "fragmentation" keyword is used to give fragmentation parameters and profile:
 - dtagSize, windowSize and FCNSize are used to define the SCHC fragmentation header
 - one and only one fragmentation mode keywork "noAck", "ackAlways" or "ackOnError".
-  These keywords are used to define some specific paramters for this mode. 
+  These keywords are used to define some specific parameters for this mode. 
   For "ackOnError" the following parameter is defined:
   - "ackBehavior" defined the ack behavior, i.e. when the Ack must be spontaneously sent
     by the receiver and therefore when the sender must listen for Ack.
