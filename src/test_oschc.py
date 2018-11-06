08f749b9c7287efefb1780fa1d6a334b81214092
Shoichi Sakane <github-shoichi@tanu.org>
2018-11-06 09:59:52 +0900

    it creates send/recv nodes explicitly. the role is no more needed.

--- src/test_oschc.py
+++ src/test_oschc.py
@@ -7,32 +7,46 @@ from base_import import *  # used for now for differing modules in py/upy
 import schc
 import simsched
 import simlayer2
+from schcrecv import SCHCProtocolReceiver
+from schcsend import SCHCProtocolSender
+from fakeschcsend import rule_from_dict
 
-rule = {
-    "rule-id": 3,
-    "mode": "ack-on-error",
+rule_as_dict = {
+    "rule-id-size": 6,
+    "rule-id": 5,
     "dtag-size": 2,
     "window-size": 5,
     "fcn-size": 3,
-    # MAX_WIND_FCN computed from "fcn-size"
+    "mode": "ack-on-error",
     "tile-size": 30,
     "mic-algorithm": "crc32",
-    "ack-after-recv-all1": True,
+
+#    "rule-id": 3,
+#    "ack-mode": "ack-after-recv-all1"
 }
-# XXX: make a namedtuple?
+
+rule = rule_from_dict(rule_as_dict)
 
 # XXX: create a real class SimulNode and Simul
-def make_node(scheduler, role="sender"):
+def make_send_node(scheduler):
+    config = {"a":0}
+    mac = simlayer2.SimulLayer2(scheduler)
+    protocol = SCHCProtocolSender(config, scheduler, mac)
+    # protocol.rulemanager.add_rule(...) ???
+    protocol.set_frag_rule(rule)
+    return mac, protocol
+
+def make_recv_node(scheduler):
     config = {}
     mac = simlayer2.SimulLayer2(scheduler)
-    protocol = schc.SCHCProtocol(config, scheduler, mac, role=role)
+    protocol = SCHCProtocolReceiver(config, scheduler, mac)
     # protocol.rulemanager.add_rule(...) ???
     protocol.set_frag_rule(rule)
     return mac, protocol
 
 scheduler = simsched.SimulScheduler()
-m0, p0 = make_node(scheduler)
-m1, p1 = make_node(scheduler, role="receiver")
+m0, p0 = make_send_node(scheduler)
+m1, p1 = make_recv_node(scheduler)
 
 m1.add_link(m0)
 m0.add_link(m1)
