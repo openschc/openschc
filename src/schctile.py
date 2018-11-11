fcf4f691f65725319ceab3ebb3145884929ed565
Shoichi Sakane <github-shoichi@tanu.org>
2018-11-12 06:42:53 +0900

    s/tile_list/all_tiles/

--- src/schctile.py
+++ src/schctile.py
@@ -13,7 +13,7 @@ class TileList():
         self.rule = rule
         t_size = rule.tile_size
         t_init_num = schcmsg.get_MAX_WIND_FCN(rule)
-        self.tile_list = []
+        self.all_tiles = []
         w_num = 0
         t_num = t_init_num
         # make tiles
@@ -26,7 +26,7 @@ class TileList():
                  for _ in range(num_full_size_tiles) ]
         if last_tile_size > 0:
             tiles.append(bb.get_bits_as_buffer(last_tile_size))
-        # make a tile_list
+        # make a all_tiles
         for t in tiles:
             tile_obj = {
                     "w-num": w_num,
@@ -35,27 +35,27 @@ class TileList():
                     "sent": False,
                     "ready_to_be_sent": False,
                 }
-            self.tile_list.append(tile_obj)
+            self.all_tiles.append(tile_obj)
             if t_num == 0:
                 t_num = t_init_num
                 w_num += 1
             else:
                 t_num -= 1
-        print("DEBUG: tile_list:")
-        for i in self.tile_list:
+        print("DEBUG: all_tiles:")
+        for i in self.all_tiles:
             print("DEBUG:  ", i)
 
     def get_tiles(self, mtu_size):
         '''
         return the tiles containing the contiguous tiles fitting in mtu_size.
-        And, remaiing nb_tiles to be sent in tile_list.
+        And, remaiing nb_tiles to be sent in all_tiles.
         '''
         max_tiles = int((mtu_size - schcmsg.get_header_size(self.rule)) /
                      self.rule.tile_size)
         tiles = []
         t_prev = None
-        for i in range(len(self.tile_list)):
-            t = self.tile_list[i]
+        for i in range(len(self.all_tiles)):
+            t = self.all_tiles[i]
             if t_prev and t_prev["t-num"] + 1 < t["t-num"]:
                 break
             if t["sent"] == False:
@@ -69,14 +69,14 @@ class TileList():
             return None, 0
         # return tiles and the remaining bits
         nb_remaining_tiles = len(
-                [ _ for _ in self.tile_list if _["ready_to_be_sent"] == False ])
+                [ _ for _ in self.all_tiles if _["ready_to_be_sent"] == False ])
         return tiles, nb_remaining_tiles
 
     def get_all_tiles(self):
-        return self.tile_list
+        return self.all_tiles
 
     def update_sent_flag(self):
-        for t in self.tile_list:
+        for t in self.all_tiles:
             if t["ready_to_be_sent"] == True:
                 t["sent"] = True
 
