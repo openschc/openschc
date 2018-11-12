bceed04a133be9c9fba8b310fee83e3d7484b34a
Shoichi Sakane <github-shoichi@tanu.org>
2018-11-12 09:19:38 +0900

    added a stub code for ACK response.  for now, just sends it back to the sender without no check.

--- src/schcrecv.py
+++ src/schcrecv.py
@@ -26,6 +26,19 @@ class ReassemblerAckOnError:
         fcn_all_1 = schcmsg.get_fcn_all_1(self.rule)
         if message.fcn == fcn_all_1:
             print("ALL1 received")
+            # XXX need to check whether all fragments hsve been received.
+            cbit = 1 # XXX
+            schc_ack = schcmsg.frag_receiver_tx_all1_ack(message.rule,
+                                                         message.dtag,
+                                                         message.win,
+                                                         cbit=cbit)
+            # XXX need finalize ?
+            print("ack message:", schc_ack.packet.get_content())
+            src_dev_id = self.protocol.layer2.mac_id
+            args = (schc_ack.packet.get_content(), src_dev_id, None, None)
+            self.protocol.scheduler.add_event(0,
+                                              self.protocol.layer2.send_packet,
+                                              args)
         print("---", message.fcn)
 
 #---------------------------------------------------------------------------
