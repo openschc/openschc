896c0a4a3b88d0ab9fb44e7d8748905bf80356d4
Shoichi Sakane <github-shoichi@tanu.org>
2018-12-01 22:01:17 +0900

    added dummy decompress function.

--- src/compress.py
+++ src/compress.py
@@ -7,9 +7,18 @@ class Compression:
         self.protocol = protocol
 
     def compress(self, packet):
+        # XXX do more work
         meta_info = {}
         result = BitBuffer(packet)
         self.protocol.log("compress", "{}=>{},{}".format(
+            repr(packet), result, meta_info))
+        return BitBuffer(packet), meta_info
+
+    def decompress(self, packet):
+        # XXX do more work
+        meta_info = {}
+        result = BitBuffer(packet)
+        self.protocol.log("decompress", "{}=>{},{}".format(
             repr(packet), result, meta_info))
         return BitBuffer(packet), meta_info
 
