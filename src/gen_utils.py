48193226cb3d28cfea2282cbb9646e57f274d539
Oliv4945 <oliv4945@gmail.com>
2019-11-17 05:33:36 +0100

    Add debug on/off  option

--- src/gen_utils.py
+++ src/gen_utils.py
@@ -5,6 +5,13 @@ import pprint
 
 enable_debug_print = False
 
+def set_debug_output(state):
+    global enable_debug_print
+    if state:
+        enable_debug_print = True
+    else:
+        enable_debug_print = False
+
 def dprint(*args, **kw):
     """Debug print"""
     global enable_debug_print
