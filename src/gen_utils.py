31dae7d4334381e9a15e436ab5d158f3ed0506ae
Shoichi Sakane <github-shoichi@tanu.org>
2019-11-17 15:29:07 +0900

    fixed to use pprint.

--- src/gen_utils.py
+++ src/gen_utils.py
@@ -22,7 +22,7 @@ def dpprint(*args, **kw):
     """Debug print"""
     global enable_debug_print
     if enable_debug_print:
-        pprint(*args, **kw)
+        pprint.pprint(*args, **kw)
 
 def dtrace(*args, **kw):
     print(*args, **kw)
