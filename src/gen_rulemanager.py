a3558bc1b7fbd77840fe5d65e1b3fadbb42f1055
Shoichi Sakane <github-shoichi@tanu.org>
2019-11-20 16:40:46 +0900

    fixed the assert of Add()

--- src/gen_rulemanager.py
+++ src/gen_rulemanager.py
@@ -406,8 +406,7 @@ class RuleManager:
 
         """
 
-        #assert (dev_info == None or file == None)
-        # XXX should it be like assert (dev_info != None or file != None)
+        assert (dev_info is not None or file is not None)
 
         if file != None:
             dev_info = json.loads(open(file).read())
