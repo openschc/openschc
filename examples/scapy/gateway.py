22d35b1b78cbc5122f8eb297b56737f3177ea819
ltn22 <laurent@touta.in>
2020-11-15 10:31:58 +0100

    IPV6 packet decomp

--- examples/scapy/gateway.py
+++ examples/scapy/gateway.py
@@ -12,7 +12,7 @@ import pprint
 import binascii
 
 import socket
-
+import ipaddress
 
 class debug_protocol:
     def _log(*arg):
@@ -26,6 +26,58 @@ rm.Print()
 comp = Compressor(debug_protocol)
 decomp = Decompressor(debug_protocol)
 
+def send_scapy(fields, pkt_bb):
+    print ('send_scapy', fields)
+
+
+    pkt_data = bytearray()
+    while (pkt_bb._wpos - pkt_bb._rpos) >= 8:
+        octet = pkt_bb.get_bits(nb_bits=8)
+        pkt_data.append(octet)
+
+    print (pkt_data)
+
+    c = {}
+    for k in [T_IPV6_DEV_PREFIX, T_IPV6_DEV_IID, T_IPV6_APP_PREFIX, T_IPV6_APP_IID]:
+        v = fields[(k, 1)][0]
+        if type(v) == bytes:
+            c[k] = int.from_bytes(v, "big")
+        elif type(v) == int:
+            c[k] = v
+        else:
+            raise ValueError ("Type not supported")
+        
+    
+    IPv6Src = (c[T_IPV6_DEV_PREFIX] <<64) + c[T_IPV6_DEV_IID]
+    IPv6Dst = (c[T_IPV6_APP_PREFIX] <<64) + c[T_IPV6_APP_IID]
+
+    
+    IPv6Sstr = ipaddress.IPv6Address(IPv6Src)
+    IPv6Dstr = ipaddress.IPv6Address(IPv6Dst)
+    
+    IPv6Header = IPv6 (
+        version= fields[(T_IPV6_VER, 1)][0],
+        tc     = fields[(T_IPV6_TC, 1)][0],
+        fl     = fields[(T_IPV6_FL, 1)][0],
+        nh     = fields[(T_IPV6_NXT, 1)][0],
+        hlim   = fields[(T_IPV6_HOP_LMT, 1)][0],
+        src    =IPv6Sstr.compressed, 
+        dst    =IPv6Dstr.compressed
+    ) 
+
+    if fields[(T_IPV6_NXT, 1)][0] == 58: # ICMPv6
+        ICMPv6Header = ICMPv6EchoReply(
+            id = fields[(T_ICMPV6_IDENT, 1)][0],
+            seq =  fields[(T_ICMPV6_SEQNO, 1)][0],
+            data = pkt_data
+        )
+    full_header = IPv6Header/ICMPv6Header
+
+    full_header.show()
+#    full_header.hexdump()
+
+    send(full_header, iface="he-ipv6")
+    
 def processPkt(pkt):
     global parser
     global rm
@@ -53,7 +105,8 @@ def processPkt(pkt):
                     #print (rule)
 
                     phd = decomp.decompress(schc_bb, rule, recv_dir)
-                    print (phd)
+                    
+                    send_scapy(phd, schc_bb)
         
     elif pkt.getlayer(IP).version == 6 : # regular IPv6trafic to be compression
 
