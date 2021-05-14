29f44ad09007443de22f0f2953fb3dae8d868e1c
Laurent Toutain <laurent@touta.in>
2021-05-14 12:12:39 +0200

    test

--- examples/scapy/ping_core.py
+++ examples/scapy/ping_core.py
@@ -4,6 +4,7 @@ sys.path.insert(1, '../../src/')
 
 from scapy.all import *
 import scapy.contrib.coap as scapy_coap
+from scapy_connection import *
 
 
 import gen_rulemanager as RM
@@ -11,7 +12,6 @@ import compr_parser as parser
 from compr_core import *
 import protocol
 from protocol import SCHCProtocol
-from scapy_connection import *
 from gen_utils import dprint, sanitize_value
 
 
