d9f57d046fe5181fe8efb279c4dd551e277c68ed
Marino Martinez <34484710+MarinoMtz@users.noreply.github.com>
2021-12-01 18:04:05 +0100

    Create bacnet.py

---
+++ bacnet.py
@@ -0,0 +1,31 @@
+# to save as a pcap: sudo tcpdump -i en0 -w test.pcap
+# python3.9 -m pip install scapy
+from scapy.all import *
+import os
+os.sys.path.append('../../src')
+
+import compr_parser as parser
+from compr_core import *
+
+import binascii
+
+class debug_protocol:
+    def _log(*arg):
+        print (arg)
+
+parse = parser.Parser(debug_protocol)
+
+sequence = rdpcap("/home/ivan/Downloads/atomic-read-file-50.cap")
+
+dst = "192.168.0.8"
+src = "192.168.0.17"
+
+for pkt in sequence:
+    if pkt.haslayer(IP):
+        if pkt[IP].fields['dst'] == "192.168.0.8":
+            dir = T_DIR_UP
+        else:
+            dir = T_DIR_DW
+
+        a, b, c = parse.parse(pkt=bytes(pkt)[18:], direction=dir, start="IPv6")
+        print (a, b, c)
