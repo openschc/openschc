c45043cef6a865cb7f92e9d803aaf6b2382d9893
IvanMtz <ivamarti@insa-rennes.fr>
2021-12-02 23:08:32 +0100

    reading pcap from simulation

--- examples/bacnet.py
+++ examples/bacnet.py
@@ -16,7 +16,7 @@ class debug_protocol:
 
 parse = parser.Parser(debug_protocol)
 compressor = Compressor(debug_protocol)
-#decompressor = Decompressor(debug_protocol)
+decompressor = Decompressor(debug_protocol)
 
 sequence = rdpcap("atomic-read-file-50.cap")
 
@@ -27,23 +27,42 @@ rm = RM.RuleManager()
 rm.Add(file="ipv4.json")
 rm.Print()
 
-# for pkt in sequence:
-#     if pkt.haslayer(IP):
-#         if pkt[IP].fields['dst'] == "192.168.0.8":
-#             dir = T_DIR_UP
-#         else:
-#             dir = T_DIR_DW
-#
-#         a, b, c = parse.parse(pkt=bytes(pkt)[14:], layers=["IPv4", "UDP"], direction=dir, start="IPv4")
-#         r, d = rm.FindRuleFromPacket(a, direction=dir, failed_field=False)
-#         #print (a, b, c)
-#         #print(d)
-#         if r:
-#             schc_pkt = compressor.compress(r, a, b, direction=dir)
-#             print (len(schc_pkt._content), len(bytes(pkt)[14:]))
-#             #print ("{} {}".format(len(schc_pkt._content)), '=')
-#             #r2 = rm.FindRuleFromSCHCpacket(schc_pkt, "udp:90.27.174.128:8888")
-#             #print (r2)
-#             #orig = decompressor.decompress(schc_pkt, r2, direction=dir)
-#             #print (orig)
+for pkt in sequence:
+    if pkt.haslayer(IP):
+        if pkt[IP].fields['dst'] == "192.168.0.8":
+            dir = T_DIR_UP
+        else:
+            dir = T_DIR_DW
 
+        a, b, c = parse.parse(pkt=bytes(pkt)[14:], layers=["IPv4", "UDP"], direction=dir, start="IPv4")
+        r, d = rm.FindRuleFromPacket(a, direction=dir, failed_field=False)
+        #print (a, b, c)
+        #print(d)
+        if r:
+            schc_pkt = compressor.compress(r, a, b, direction=dir)
+            print (len(schc_pkt._content), len(bytes(pkt)[14:]))
+            #print ("{} {}".format(len(schc_pkt._content)), '=')
+            #r2 = rm.FindRuleFromSCHCpacket(schc_pkt, "udp:90.27.174.128:8888")
+            #print (r2)
+            #orig = decompressor.decompress(schc_pkt, r2, direction=dir)
+            #print (orig)
+
+sequence1 = rdpcap("Test_bacnet_simulator.pcap")
+
+dst = "131.254.252.105"
+src = "131.254.252.119"
+
+for pkt in sequence:
+    if pkt.haslayer(IP):
+        if pkt[IP].fields['dst'] == "192.168.0.8":
+            dir = T_DIR_UP
+        else:
+            dir = T_DIR_DW
+
+        a, b, c = parse.parse(pkt=bytes(pkt)[14:], layers=["IPv4", "UDP"], direction=dir, start="IPv4")
+        r, d = rm.FindRuleFromPacket(a, direction=dir, failed_field=False)
+        #print (a, b, c)
+        #print(d)
+        if r:
+            schc_pkt = compressor.compress(r, a, b, direction=dir)
+            print (len(schc_pkt._content), len(bytes(pkt)[14:]))
