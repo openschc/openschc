206c54aa25c1caaf9f3ffc5f385e1e5f978bde2a
Cedric Adjih <cedric.adjih@inria.fr>
2022-02-16 14:06:57 +0100

    cleaned some examples

--- src/test_compress.py
+++ src/test_compress.py
@@ -3,46 +3,15 @@ from compr_core import *
 from compr_parser import *
 from gen_rulemanager import *
 
-import json
-
 import pprint
 import binascii
 
-# rule_base = json.loads(open("example/comp-rule-100.json").read())
-# new_rule_set = []
-# for i in rule_base["Compression"]: # rule_set entry has been removed
-#     rule = {}
-#     for k,v in i.items():
-#         if isinstance(v, str):
-#             rule[k.upper()] = v.upper()
-#         else:
-#             rule[k.upper()] = v
-#     new_rule_set.append(rule)
-#
-# rule_base["compression"] = new_rule_set # rule_set has been removed
-# context = { "comp": rule_base }
-
-#pkt = open("test/icmpv6.dmp", "rb").read()
-#input_bbuf = BitBuffer(pkt)
 
 class debug_protocol:
     def _log(*arg):
         print(*arg)
 
 p = Parser(debug_protocol)
-#p.parse(pkt, T_DIR_UP)
-#c = Compressor(debug_protocol)
-#c.init()
-#output_bbuf = c.compress(context, input_bbuf, "DW")
-
-#print("---------------------------------------------------------------------------")
-
-#d = Decompressor(debug_protocol)
-#c.init()
-#decoded_bbuf = d.decompress(context, output_bbuf, "DW")
-#print("decoded :", decoded_bbuf)
-
-#print("original:", input_bbuf)
 
 coap = bytearray(b"""`\
 \x12\x34\x56\x00\x1e\x11\x1e\xfe\x80\x00\
@@ -55,9 +24,8 @@ foo\x03bar\x06ABCD==Fk=eth0\xff\x84\x01\
 
 print (b2hex(coap))
 
-v= p.parse(coap, T_DIR_UP)  # or T_DIR_DW
+v = p.parse(coap, T_DIR_UP)  # or T_DIR_DW
 pprint.pprint(v[0])
-# print(binascii.hexlify(v[1]))
 
 RM = RuleManager(log=debug_protocol)
 RM.Add(file="../examples/configs/comp-rule-100.json")
