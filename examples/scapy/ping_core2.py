1b080a8b9a7ce5db5a966fd86fcbd6b662994592
IvanMtz <ivamarti@insa-rennes.fr>
2022-02-24 12:07:03 +0100

    test NoAck

--- examples/scapy/ping_core2.py
+++ examples/scapy/ping_core2.py
@@ -46,7 +46,7 @@ def processPkt(pkt):
                     uncomp_pkt = schc_machine.schc_recv(device_id=other_end, schc_packet=schc_pkt)                       
                     if uncomp_pkt != None:
                         uncomp_pkt[1].show()
-                        send(uncomp_pkt, iface="he-ipv6") 
+                        send(uncomp_pkt[1], iface="he-ipv6") 
             elif ip_proto==41:
                 schc_machine.schc_send(bytes(pkt)[34:])
 
