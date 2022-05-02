032408586b8fb58f8c97b400a480cb4b7f7d0247
MarinoMtz <ivamarti@insa-rennes.fr>
2022-05-02 14:13:11 +0200

    adding core_id and device_id in context

--- examples/scapy/ping_device_AoE.py
+++ examples/scapy/ping_device_AoE.py
@@ -78,7 +78,7 @@ def processPkt(pkt):
                     else:
                         # None when the reassambly + decompressing process is not finished and [device_id, decompressed packet in bytes] when All-1
                         print("core address", addr)
-                        core_id = 'udp:'+ str(addr) +":"+str(socket_port)
+                        core_id = 'udp:'+ str(addr[0]) +":"+str(socket_port)
                         r = schc_machine.schc_recv(core_id = core_id, device_id=device_id, schc_packet=schc_pkt) 
                         print ('r = ', r)
                         if r is not None: #The SCHC machine has reassembled and decompressed the packet
