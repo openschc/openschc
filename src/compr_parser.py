a0d7e9757015a1e06291b0f5a0c32b1f16c3fbc5
Kaustubh Joshi <kaustubh@Kaustubhs-MacBook-Air.local>
2022-05-19 11:00:05 +0200

    made some diagnostic changes to protocol

--- src/compr_parser.py
+++ src/compr_parser.py
@@ -376,7 +376,7 @@ class Unparser:
         
         full_packet = L3header / L4header / Raw(load=coap_h)
 
-        print(binascii.hexlify(byte(full_packet)))
+        print(binascii.hexlify(bytes(full_packet)))
         return full_packet
 
         
