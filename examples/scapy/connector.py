c151cb5c9a984f436ca8ae0333607757a699bdd6
Laurent Toutain <laurent@touta.in>
2022-10-07 03:13:10 +0200

    chirpstack

--- examples/scapy/connector.py
+++ examples/scapy/connector.py
@@ -119,6 +119,7 @@ def recv_data(sock):
 
             answer = {
                 "deviceQueueItem": {
+                    "confirmed": false,
 		            "data": base64.b64encode(content).decode('utf-8'),
                     "fPort": fport
                 }
