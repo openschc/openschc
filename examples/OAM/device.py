deaf150017ceac235559b91c3147a5540b3c55b3
Laurent Toutain <laurent@touta.in>
2023-02-28 10:10:24 +0100

    starting platform

--- examples/OAM/device.py
+++ examples/OAM/device.py
@@ -1,6 +1,7 @@
 """Simple device generating periodically SCHC rules"""
 
 import socket
+import select
 import struct
 import time 
 import binascii
@@ -15,15 +16,21 @@ ruleID = 100 # CoAP simple message
 MID = 1
 while True:
 
+    rs = select.select([tunnel], [], [], 30)
+
+    print(rs)
+    if rs[0]:
+        data, addr = socket.readfrom (rs[0])
+        print (binascii.hexlify(data), addr)
+
     SCHCmsg = struct.pack("!BB", ruleID, MID) + b'\x00'
 
     if MID == 255:
         MID = 1
     else:
         MID += 1
+    
 
     tunnel.sendto(SCHCmsg, ("51.91.121.182", 0x5C4C))
     print ("send ", binascii.hexlify(SCHCmsg))
-
-    time.sleep(30)
 
