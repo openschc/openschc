75c425bad412f7bcd24e8fa1c3e8a33a21ff844b
Laurent Toutain <laurent@touta.in>
2023-04-18 09:28:31 +0200

    timestamp

--- examples/OAM/core.py
+++ examples/OAM/core.py
@@ -68,7 +68,7 @@ schc_machine = SCHCProtocol(
     system=system,           # define the scheduler
     layer2=lower_layer,      # how to send messages
     role=POSITION,           # DEVICE or CORE
-    verbose = True)         
+    verbose = False)         
 schc_machine.set_rulemanager(rm)
 
 sniff(prn=processPkt, iface=[INTERFACE, "ens3"]) # scappy cannot read multiple interfaces
