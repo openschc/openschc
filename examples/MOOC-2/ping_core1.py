27f46af8f0f7b1c3faf52cef8f6fa9455f2fee9b
Laurent Toutain <laurent@touta.in>
2023-12-28 07:24:59 +0100

    skelethon

--- examples/MOOC-2/ping_core1.py
+++ examples/MOOC-2/ping_core1.py
@@ -31,7 +31,7 @@ def processPkt(pkt):
                 print(other_end)
 
                 full_packet = schc_machine.schc_recv(schc_packet=SCHC_pkt, device_id=other_end)
-                hexdump(full_packet)
+                full_packet.show()
 
                 
 
