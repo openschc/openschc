e6e766d6e62a939a41d9ae40970adbe4eb172560
Laurent Toutain <laurent@touta.in>
2023-12-28 10:41:09 +0100

    skelethon

--- examples/MOOC-2/ping_dev1.py
+++ examples/MOOC-2/ping_dev1.py
@@ -42,7 +42,7 @@ def processPkt(pkt):
                 origin, full_packet = schc_machine.schc_recv(
                                    schc_packet=SCHC_pkt, 
                                    device_id=deviceID, 
-                                   iface='eth0',
+                                   iface='lo',
                                    verbose=True)
 
 # Start SCHC Machine
