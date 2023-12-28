1c4a64062685a9fa2f1f3a84de60695071a439f0
Laurent Toutain <laurent@touta.in>
2023-12-28 05:16:46 +0100

    skelethon

---
+++ examples/MOOC-2/ping_dev.py
@@ -0,0 +1,26 @@
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '../../src/')
+import gen_rulemanager as RM
+
+
+rm = RM.RuleManager()
+rm.Add(file="icmp-bi.json")
+rm.Print()
+
+import socket
+import binascii
+
+tunnel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tunnel.bind ("0.0.0.0", 8888) # same port as in the DeviceID
+
+while True:
+    SCHC_pkt = tunnel.read(1000)
+    print ("SCHC Packet:", binascii.hexlify(SCHC_pkt)  )
+
+
+
+
+
+
+ 
