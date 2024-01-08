036223f4835666fd995905365c514a2107c656f4
Laurent Toutain <laurent@touta.in>
2024-01-08 16:13:29 +0100

    fix networking

--- examples/MOOC-2/ping_dev1.py
+++ examples/MOOC-2/ping_dev1.py
@@ -36,6 +36,7 @@ def processPkt(pkt):
         e_type = pkt.getlayer(Ether).type
         if e_type == 0x86dd:
             print ("get IPv6 packet")
+            pkt.show()
             if core_id:
                 schc_machine.schc_send(bytes(pkt)[14:], core_id = core_id)
         elif e_type == 0x0800:
