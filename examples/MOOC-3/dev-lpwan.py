a7551f0027ef7ae0f791c7f4ebcf845e0673cf2e
Laurent Toutain <laurent@touta.in>
2024-02-01 16:58:14 +0100

    1st LPWAN dev

---
+++ examples/MOOC-3/dev-lpwan.py
@@ -0,0 +1,50 @@
+import socket
+import cbor2 as cbor
+import random
+import time
+import struct
+import binascii
+
+CORE_SCHC = ("10.0.0.21", 0x5C4C)
+MID_LENGTH=3
+KNOWN_URI = ["/temp", "/humi", "/pres"]
+
+MID = 1
+
+tunnel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def coap_send_measurement(value, uri):
+    global MID
+
+    if uri not in KNOWN_URI:
+        print("unknown URI")
+        return None
+
+    schc_residue = (0x00 & 0b0000_0111) << 5 | \
+                   (MID & 0b0000_0111) << 2 | \
+                   KNOWN_URI.index(uri) & 0b0000_0011
+    
+    if MID == 0b0000_0111:
+        MID = 1
+    else:
+        MID += 1
+    
+    schc_pkt = struct.pack("!B", schc_residue) + cbor.dumps(value)
+    print (binascii.hexlify(schc_pkt))
+    tunnel.sendto(schc_pkt, CORE_SCHC)
+
+temp = humi = pres = 0
+
+while True:
+    m = random.randint(0,2)
+    if m == 0:
+        temp += random.randint (-5, +5)
+        coap_send_measurement(temp, "/temp")
+    elif m == 1:
+        humi += random.randint (-10, +10)
+        coap_send_measurement(humi, "/humi")
+    elif m == 3:
+        pres += random.randint (-10, +10)
+        coap_send_measurement(pres, "/pres")
+
+    time.sleep(random.randint(1, 7))
