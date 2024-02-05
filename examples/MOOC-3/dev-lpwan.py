656865762c8f0f6fb17e5a7632d3d0efed8d750e
Laurent Toutain <laurent@touta.in>
2024-02-05 10:12:02 +0100

    ping natif

--- examples/MOOC-3/dev-lpwan.py
+++ examples/MOOC-3/dev-lpwan.py
@@ -45,6 +45,7 @@ def coap_send_measurement(value, uri):
 
 def wait_ack():
     readable, writable, exp = select.select ([tunnel], [], [], 0.1)
+    print ("readable", readable)
     if readable is not []:
         msg = tunnel.recv(1000)
         print ("Get downlink", binascii.hexlify(msg))
