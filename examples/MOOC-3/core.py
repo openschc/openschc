123b8dda611bc99386328eeac6661ae7f323182c
Laurent Toutain <laurent@touta.in>
2024-02-08 09:48:44 +0100

    ping natif

--- examples/MOOC-3/core.py
+++ examples/MOOC-3/core.py
@@ -22,9 +22,7 @@ def processPkt(pkt):
     if pkt.getlayer(Ether) != None: 
         e_type = pkt.getlayer(Ether).type
         if e_type == 0x86dd:
-            hexdump(pkt)
-            pkt.show()
-            schc_machine.schc_send(bytes(pkt)[14:], verbose=True)
+            schc_machine.schc_send(bytes(pkt)[14:], verbose=False)
         elif e_type == 0x0800:
             if pkt[IP].proto == 17 and pkt[UDP].dport == 0x5C4C:
                 # got a packet in the socket
