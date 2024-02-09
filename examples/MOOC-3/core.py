5ff46553bf6997f1d3d185a90d8b836499681236
Laurent Toutain <laurent@touta.in>
2024-02-09 09:42:06 +0100

    ping natif

--- examples/MOOC-3/core.py
+++ examples/MOOC-3/core.py
@@ -46,10 +46,7 @@ schc_machine.set_rulemanager(rm)
 scheduler = schc_machine.system.get_scheduler()
 tunnel = schc_machine.get_tunnel()
 
-while True:
-    sniff(prn=processPkt, iface=["eth0", "eth1", "lo"], timeout=0.1) 
-    print ('-', end="")
-    scheduler.run(session=schc_machine)
+sniff(prn=processPkt, iface=["eth0", "eth1", "lo"], timeout=0.1) 
 
 
 
