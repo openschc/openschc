0ceb05568c25b968a492769006d6cb19d662dd20
Laurent Toutain <laurent@touta.in>
2024-02-15 14:46:06 +0100

    underwater

---
+++ examples/MOOC-4/coap-client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Christian Amsüss and the aiocoap contributors
+#
+# SPDX-License-Identifier: MIT
+
+"""This is a usage example of aiocoap that demonstrates how to implement a
+simple client. See the "Usage Examples" section in the aiocoap documentation
+for some more information."""
+
+import logging
+import asyncio
+
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri='coap://[aaaa::1]]/temp')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
+
+if __name__ == "__main__":
+    asyncio.run(main())
