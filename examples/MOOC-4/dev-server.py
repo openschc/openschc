606dc44785a85c1de7fa402a8236914199ef02bc
Laurent Toutain <laurent@touta.in>
2024-02-27 10:10:48 +0100

    work on

--- examples/MOOC-4/dev-server.py
+++ examples/MOOC-4/dev-server.py
@@ -1,8 +1,18 @@
 import socket
+import binascii
+
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '../../src/')
+
+import gen_bitarray as ba
 
 tunnel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tunnel.bind(('0.0.0.0', 8888))
 
 while True:
     data, addr = tunnel.recvfrom(1500)
-    print (data, addr)
+    print (binascii.hexlify(data))
+
+    print (bin(data[0]))
+    
