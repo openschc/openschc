d2cda70029b3264a7685b9a3cd392b93dc754ea5
Laurent Toutain <laurent@touta.in>
2024-02-27 10:07:21 +0100

    work on

--- examples/MOOC-4/dev-server.py
+++ examples/MOOC-4/dev-server.py
@@ -4,5 +4,5 @@ tunnel = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tunnel.bind(('0.0.0.0', 8888))
 
 while True:
-    data, addr = tunnel.recv_into(1500)
+    data, addr = tunnel.recvfrom(1500)
     print (data, addr)
