625c6b6e890f1d83f43a6c732b2f5a9d7573a58a
Laurent Toutain <laurent@touta.in>
2024-05-24 08:44:59 +0200

    frag

--- examples/MOOC-5/ping_dev1.py
+++ examples/MOOC-5/ping_dev1.py
@@ -122,6 +122,8 @@ while True:
                         verbose=True)
             
                 print (Echoreply)
+            else:
+                print ("Received fragment, reassembly buffer incomplete.")
 
     time.sleep(0.1)
 
