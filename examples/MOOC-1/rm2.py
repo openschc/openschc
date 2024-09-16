6b164a730b9eff3a71231187543b9400cc07d29c
Laurent Toutain <laurent@touta.in>
2024-09-16 18:08:57 +0200

    missing file

---
+++ examples/MOOC-1/rm2.py
@@ -0,0 +1,22 @@
+from gen_rulemanager import *
+
+RM = RuleManager()
+
+rule1100 =   {
+ "RuleID" : 12,
+ "RuleIDLength" : 4,
+ "Compression" : []
+}
+
+rule001100 =   {
+ "RuleID" : 12,
+ "RuleIDLength" : 6,
+ "Fragmentation" : {
+   "FRMode": "noAck",
+   "FRDirection" : "UP"
+ }
+}
+
+RM.Add(dev_info=[rule1100, rule001100])
+
+RM.Print()
